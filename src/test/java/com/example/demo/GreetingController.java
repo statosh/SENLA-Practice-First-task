e390ac30c5c4b672e0aeb596ec7c8dbddc730614
Stavtsev Vladislav <147432896+statosh@users.noreply.github.com>
2026-01-28 23:20:54 +0300

    Add files via upload

---
+++ src/test/java/com/example/demo/GreetingController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greet")
+public class GreetingController {
+
+    @GetMapping
+    public String greet() {
+        return "Hello";
+    }
+
+}
