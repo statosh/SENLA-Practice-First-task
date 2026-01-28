10991cf5bd1289c2faa5087fc59a7b24f65997e0
Stavtsev Vladislav <147432896+statosh@users.noreply.github.com>
2026-01-28 23:24:28 +0300

    Add files via upload

---
+++ src/test/java/com/example/demo/User.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String username;
+    private List<Post> posts;
+
+    public User() {
+        posts = new ArrayList<>();
+    }
+
+    public User(String username, List<Post> posts) {
+        this.username = username;
+        this.posts = posts == null ? new ArrayList<>() : posts;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+}
