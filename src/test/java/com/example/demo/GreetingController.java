d30b96ce8538b0c681c554a412cf2eb7440fd12f
Stavtsev Vladislav <147432896+statosh@users.noreply.github.com>
2026-01-28 23:23:23 +0300

    Add files via upload

--- src/test/java/com/example/demo/GreetingController.java
+++ src/test/java/com/example/demo/GreetingController.java
@@ -2,15 +2,27 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greet")
 public class GreetingController {
 
     @GetMapping
-    public String greet() {
-        return "Hello";
+    public String greet(@RequestParam("name") String name) {
+        return "Hello, " + name;
+    }
+
+    @GetMapping("/full")
+    public String fullGreeting(@RequestParam("name") String name, @RequestParam("surname") String surname) {
+        return "Nice to meet you, " + name + " " + surname;
+    }
+
+    @GetMapping("/{name}")
+    public String greetWithPathVariable(@PathVariable("name") String name) {
+        return "Hello, " + name;
     }
 
 }
